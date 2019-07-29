a4e2740173e23cc065cab33701fa7af76d8a7f46
Rodrigo Portilio <rodrigo.portilio@outlook.com>
2019-07-29 20:41:44 -0300

    Starting Handler

---
+++ PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Flunt.Notifications;
+using PaymentContext.Domain.Commands;
+using PaymentContext.Shared.Commands;
+using PaymentContext.Shared.Handlers;
+
+namespace PaymentContext.Domain.Handlers
+{
+    public class SubscriptionHandler : Notifiable, IHandler<CreateBoletoSubscriptionCommand>
+    {
+        public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PaymentContext.Shared/Handlers/IHandler.cs
+++ PaymentContext.Shared/Handlers/IHandler.cs
@@ -4,6 +4,6 @@ namespace PaymentContext.Shared.Handlers
 {
     public interface IHandler<T> where T : ICommand
     {
-        
+        ICommandResult Handle(T command);
     }
 }
